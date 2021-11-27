8e3932a98414559c95202c70dbc8b05fc3fd3ded
John Contreras <john.contreras@aveco.com>
2021-11-26 23:21:39 -0500

    añadiendo funcionalidad de instrucciones y funcionalidad shuffle

--- poo.js
+++ poo.js
@@ -41,6 +41,16 @@ class Board{
 
     restart(){
         console.log("Reiniciando el juego");
+        var lista = [1,2,3,4,5,6,7,8,""];
+        lista = lista.sort(function() {return Math.random() - 0.5});
+        console.log (lista)
+        var j=0;
+        for (let i = 0 ; i < lista.length; i++ )
+            {
+                var uno = document.getElementById('b'+i);
+                uno.innerHTML = lista[i];           
+            }    
+
     }
 
 
@@ -92,9 +102,15 @@ class Square{
 
 
 
+
 //instancias Board
 
-const Board1 = new Board(5, "Juegue limpio y rapido");
+const Board1 = new Board(5,
+"* El objetivo es colocar los números 1,2,3 en la primera fila, 4,5,6 en la segunda y 7,8 en la tercera.\n " 
++"* La celda vacía debe quedar en la esquina inferior derecha. \n"
++"* Al presionar un botón con numero si la celda presionada, tiene una celda vacía vecina, esta celda ocupará la celda vacía. \n"
++"* Si se presiona una celda sin tener la celda vacía de vecina, se contará como un movimiento."
+);
 console.log(Board1.movementsCounter);
 Board1.movementsCounter=1000
 console.log(Board1.movementsCounter);
@@ -110,3 +126,20 @@ const square1 = new Square(1, "Verde", "F");
 console.log(square1.color)
 console.log(square1.ID)
 console.log(square1.isSwapable)
+
+
+
+//Reiniciar el tablero
+Board1.restart();
+
+//Obtener las instrucciones
+function showInstructions(){
+    alert(Board1.instructions);
+}
+
+function restart(){
+    Board1.restart();
+}
+
+
+
